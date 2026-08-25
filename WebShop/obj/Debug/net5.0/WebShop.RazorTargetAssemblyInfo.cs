e7992aec3859b8ada3eebddee489b8379c84621d
Nguyễn Hữu Quốc Duy <huuduyaz123@gmail.com>
2026-08-25 21:44:31 +0700

    Push project WebShop

--- WebShop/obj/Debug/net5.0/WebShop.RazorTargetAssemblyInfo.cs
+++ WebShop/obj/Debug/net5.0/WebShop.RazorTargetAssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebShop")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd99212f113738c9471f1f1814d7eb4acd84f981")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebShop.Views")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
